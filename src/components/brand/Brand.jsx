d7701dc3dd7a76585aea5c55960f6158d19b3dbd
Wojciech Marek <marek.wojciech@outlook.com>
2022-02-23 18:26:38 +0100

    Brand - implemetation

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./brand.css";
+import { atlassian, dropbox, google, shopify, slack } from "./imports";
+
+const socials = [
+  { name: "google", logo: google },
+  { name: "dropbox", logo: dropbox },
+  { name: "shopify", logo: shopify },
+  { name: "slack", logo: slack },
+  { name: "atlassian", logo: atlassian },
+];
 
 const Brand = () => {
-  return <div>Article</div>;
+  return (
+    <div className="gpt3__brand section__padding">
+      {socials.map((social) => {
+        return (
+          <div>
+            <img src={social.logo} alt={social.name} />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Brand;
